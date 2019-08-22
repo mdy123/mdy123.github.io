1f5b739be6441a96709d91897de43c8d572c4c83
mdy123 <a_a_aung@yahoo.com>
2019-08-22 11:31:32 -0700

    Update radio_button.dart
    radio button flutter widget

--- flutter/radio_button.dart
+++ flutter/radio_button.dart
@@ -23,14 +23,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  final _foodType = ['herbivore', 'carnivore', 'omnivore'];
+  final _foodType = ['Herbivore', 'Carnivore', 'Omnivore'];
   final _animal = {
-    'snake': [0, 1, 2],
-    'lion': [0, 1, 2],
-    'bird': [0, 1, 2]
+    'Snake': [0, 1, 2],
+    'Lion': [0, 1, 2],
+    'Bird': [0, 1, 2],
+    'Dog': [0, 1, 2],
+    'Cat': [0, 1, 2],
+    'Tiger': [0, 1, 2],
+    'Deer': [0, 1, 2]
   };
-
-  final _selected = [0, 0, 0];
+  
+  final _selected = List.generate(7, (i) => 0);
 
   @override
   Widget build(BuildContext context) {
@@ -40,38 +44,41 @@ class _MyAppState extends State<MyApp> {
         Expanded(
           flex: 8,
           child: Container(
-            child: Column(
-              children: <Widget>[
-                ListTile(
-                  leading: Text(''),
-                  title: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: <Widget>[
-                      for (var x = 0; x < _foodType.length; x++)
-                        Text(_foodType[x])
-                    ],
-                  ),
-                ),
-                for (var x = 0; x < _animal.length; x++)
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Column(
+                children: <Widget>[
                   ListTile(
-                    leading: Text(_animal.keys.toList()[x]),
+                    leading: Text(''),
                     title: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: <Widget>[
-                        for (var y = 0; y < 3; y++)
-                          Radio(
-                            value: _animal[_animal.keys.toList()[x]][y],
-                            groupValue: _selected[x],
-                            onChanged: (v) {
-                              setState(() {
-                                _selected[x] = v;
-                              });
-                            },
-                          ),
+                        for (var x = 0; x < _foodType.length; x++)
+                          Text(_foodType[x])
                       ],
                     ),
-                  )
-              ],
+                  ),
+                  for (var x = 0; x < _animal.length; x++)
+                    ListTile(
+                      leading: Text(_animal.keys.toList()[x]),
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          for (var y = 0; y < 3; y++)
+                            Radio(
+                              value: _animal[_animal.keys.toList()[x]][y],
+                              groupValue: _selected[x],
+                              onChanged: (v) {
+                                setState(() {
+                                  _selected[x] = v;
+                                });
+                              },
+                            ),
+                        ],
+                      ),
+                    )
+                ],
+              ),
             ),
           ),
         ),
@@ -81,11 +88,15 @@ class _MyAppState extends State<MyApp> {
             child: SingleChildScrollView(
               scrollDirection: Axis.vertical,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   for (var x = 0; x < _animal.length; x++)
-                    Text(
-                        '${_animal.keys.toList()[x]} is ${_foodType[_selected[x]]}')
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 12),
+                      child: Text(
+                          '${_animal.keys.toList()[x]} is ${_foodType[_selected[x]]}'),
+                    ),
                 ],
               ),
             ),
