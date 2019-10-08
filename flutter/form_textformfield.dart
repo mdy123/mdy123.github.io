fb662059cbd526c2d1371e6429cba1bb437c9e98
mdy123 <a_a_aung@yahoo.com>
2019-10-07 20:08:10 -0700

    Create form_textformfield.dart

---
+++ flutter/form_textformfield.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Form TextFormField'),
+          ),
+          body: MyApp(),
+        );
+      }
+    },
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final RegExp expEmail = new RegExp(
+      r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+  final RegExp expPhone = new RegExp(r"^[2-9]\d{2} \d{3} \d{4}$");
+  final List<String> _names = ['Full Name', 'Email     ', 'Phone    '];
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          LayoutBuilder(
+            builder: (context, constraints) {
+              return Column(
+                children: <Widget>[
+                  for (var x in _names)
+                    Padding(
+                      padding: EdgeInsets.only(left: 8),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(x),
+                          Container(
+                            child: TextFormField(
+                              validator: (v) {
+                                String returnValue;
+                                switch (x.trim()) {
+                                  case 'Full Name':
+                                    {
+                                      v.isEmpty
+                                          ? returnValue =
+                                              'Please enter some text'
+                                          : returnValue = null;
+                                    }
+                                    break;
+                                  case 'Email':
+                                    {
+                                      if (v.isEmpty) {
+                                        returnValue = 'Please enter some text';
+                                      } else if (!expEmail.hasMatch(v)) {
+                                        returnValue =
+                                            'Wrong Email Format. xxx@gmail.com';
+                                      } else {
+                                        returnValue = null;
+                                      }
+                                    }
+                                    break;
+                                  case 'Phone':
+                                    {
+                                      if (v.isEmpty) {
+                                        returnValue = 'Please enter some text';
+                                      } else if (!expPhone.hasMatch(v)) {
+                                        returnValue =
+                                            'Wrong Phone Format. xxx xxx xxxx';
+                                      } else {
+                                        returnValue = null;
+                                      }
+                                    }
+                                    break;
+                                }
+                                return returnValue;
+                              },
+                            ),
+                            width: constraints.constrainWidth() / 1.5,
+                          ),
+                        ],
+                      ),
+                    ),
+                  FlatButton(
+                    color: Colors.red,
+                    onPressed: () {
+                      if (_formKey.currentState.validate())
+                        print('Complete......');
+                      //Focus.of(context).requestFocus(new FocusNode());
+                      //Focus.of(context).unfocus();
+                    },
+                    child: Text('Submit'),
+                  )
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
