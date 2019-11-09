db9e635ef252f29afba1f8291362d7438a2c03e7
mdy123 <a_a_aung@yahoo.com>
2019-11-09 13:11:46 -0800

    Update show_dialog.dart

--- flutter/show_dialog.dart
+++ flutter/show_dialog.dart
@@ -13,8 +13,39 @@ void main() {
           body: MyApp1(),
         );
       },
+      'RouteToDialog': (context) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Route to Dialog'),
+          ),
+          body: MyApp2(),
+        );
+      },
     },
   ));
+}
+
+class MyApp2 extends StatefulWidget {
+  @override
+  _MyApp2State createState() => _MyApp2State();
+}
+
+class _MyApp2State extends State<MyApp2> {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Route to Dialog'),
+      content: Text('Using Route to Dialog'),
+      actions: <Widget>[
+        FlatButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Ok')),
+        FlatButton(onPressed: () {}, child: Text('Canel')),
+      ],
+    );
+  }
 }
 
 void _showDialog(context) {
@@ -43,6 +74,9 @@ class MyApp1 extends StatefulWidget {
 }
 
 int _index = 0;
+String _title1 = 'AlertD Using AlertDialog';
+String _title2 = 'AlertD Using ShowDialog';
+String _title3 = 'AlertD Using Route';
 
 class _MyApp1State extends State<MyApp1> {
   @override
@@ -54,21 +88,37 @@ class _MyApp1State extends State<MyApp1> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  setState(() {
-                    _index = 1;
-                  });
-                },
-                child: Text('AlertD Using AlertDialog  '),
+              Container(
+                width: MediaQuery.of(context).size.width / 2.15,
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    setState(() {
+                      _index = 1;
+                    });
+                  },
+                  child: Text('$_title1'),
+                ),
               ),
-              FlatButton(
-                color: Colors.red,
-                onPressed: () {
-                  _showDialog(context);
-                },
-                child: Text('AlertD Using ShowDialog'),
+              Container(
+                width: MediaQuery.of(context).size.width / 2.15,
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    _showDialog(context);
+                  },
+                  child: Text('$_title2'),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width / 2.15,
+                child: FlatButton(
+                  color: Colors.red,
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'RouteToDialog');
+                  },
+                  child: Text('$_title3'),
+                ),
               ),
             ],
           ),
