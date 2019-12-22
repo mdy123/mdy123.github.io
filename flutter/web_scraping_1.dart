3fb270644db09a62cd053b81a1fd7e4e3b06a55c
mdy123 <a_a_aung@yahoo.com>
2019-12-22 15:10:26 -0800

    Update web_scraping_1.dart

--- flutter/web_scraping_1.dart
+++ flutter/web_scraping_1.dart
@@ -19,70 +19,71 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final HttpClient _client = HttpClient();
-  List<int> pageNumber = List.generate(5,(v)=>v+1);
-  int selectedPage =1;
+  
+  int selectedPage = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Web Scarping'),
       ),
-      body: FutureBuilder(
-          future: _client
-              .getUrl(Uri.parse('http://quotes.toscrape.com/page/$selectedPage/'))
-              .then((v) => v.close())
-              .then((resp) => resp.toList())
-              .then((v) => parser
-                  .parse(utf8.decode(v[0]))
-                  .body
-                  .querySelectorAll('.text')),
-          builder: (context, snapshot) {
-            return snapshot.hasData
-                ? SingleChildScrollView(
-                    child: Column(
-                      children: <Widget>[
-                        DropdownButton(
-                            iconEnabledColor: Colors.blue,
-
-                            value: selectedPage,
-                            items: pageNumber.map((v)=>DropdownMenuItem(
-                                value: v,
-                                child: Text('Page : $v'))
-                            ).toList(),
-                            onChanged: (v) {
-                              setState(() {
-                                selectedPage = v;
-                              });
-                              print(v);
-                            }),
-                        ListView(
-                            primary: false,
-                            shrinkWrap: true,
-                            children: ListTile.divideTiles(
-                                    color: Colors.red,
-                                    context: context,
-                                    tiles: snapshot.data
-                                        .map((v) => ListTile(
-                                              title: Text(
-                                                '${v.text}',
-                                              ),
-                                            ))
-                                        .cast<Widget>()
-                                        .toList())
-                                .toList()),
-                      ],
-                    ),
-                  )
-                : Container(
-                    height: MediaQuery.of(context).size.height,
-                    child: Center(
-                      child: CircularProgressIndicator(
-                        strokeWidth: 15,
-                        backgroundColor: Colors.blue,
-                      ),
-                    ),
-                  );
-          }),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: <Widget>[
+            DropdownButton(
+                iconEnabledColor: Colors.blue,
+                value: selectedPage,
+                items: List.generate(5, (idx) => idx + 1)
+                    .map((v) =>
+                        DropdownMenuItem(value: v, child: Text('Page : $v')))
+                    .toList(),
+                onChanged: (v) {
+                  setState(() {
+                    selectedPage = v;
+                  });
+                }),
+            FutureBuilder(
+                future: _client
+                    .getUrl(Uri.parse(
+                        'http://quotes.toscrape.com/page/$selectedPage/'))
+                    .then((v) => v.close())
+                    .then((resp) => resp.toList())
+                    .then((v) => parser
+                        .parse(utf8.decode(v[0]))
+                        .body
+                        .querySelectorAll('.text')),
+                builder: (context, snapshot) {
+                  return snapshot.hasData
+                      ? ListView(
+                          primary: false,
+                          shrinkWrap: true,
+                          children: ListTile.divideTiles(
+                                  color: Colors.red,
+                                  context: context,
+                                  tiles: snapshot.data
+                                      .map((v) => ListTile(
+                                            title: Text(
+                                              '${v.text}',
+                                            ),
+                                          ))
+                                      .cast<Widget>()
+                                      .toList())
+                              .toList())
+                      : Container(
+                          height: MediaQuery.of(context).size.height,
+                          child: Center(
+                            child: CircularProgressIndicator(
+                              strokeWidth: 15,
+                              backgroundColor: Colors.blue,
+                            ),
+                          ),
+                        );
+                }),
+          ],
+        ),
+      ),
     );
   }
 }
