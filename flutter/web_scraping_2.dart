16558b3cfe875fa8b3106ac3ed17bcad1f0f84f3
mdy123 <a_a_aung@yahoo.com>
2019-12-30 10:23:55 -0800

    Update web_scraping_2.dart

--- flutter/web_scraping_2.dart
+++ flutter/web_scraping_2.dart
@@ -1,4 +1,9 @@
- void transCode(String x) async {
+import 'package:html/parser.dart';
+
+import 'dart:io';
+import 'dart:convert';
+
+void transCode(String x) async {
     var req = await _client
         .getUrl(Uri.parse(x));
     var resp = await req.close();
@@ -44,8 +49,65 @@
     var pge = await resp.toList();
 
     var bdy = parse(utf8.decode(pge[1])).body;
-    //('${bdy.querySelectorAll('.blueRow')}');
-    //print('cde${bdy.querySelectorAll('a')[1].attributes['href']}');
-    transCode('cde${bdy.querySelectorAll('a')[1].attributes['href']}');
+    
+    //print('https://abc.com${bdy.querySelectorAll('a')[1].attributes['href']}');
+    transCode('https://abc.com${bdy.querySelectorAll('a')[1].attributes['href']}');
+
+  }
+
+//**********************************************************************************
+
+
+  void transCode(String x) async {
+    var req = await _client
+        .getUrl(Uri.parse(x));
+    var resp = await req.close();
+    var pge = await resp.toList();
+    var bdy = parse(utf8.decode(pge[0])).body;
+
+    print('${bdy.querySelector('issuerName').text} ${bdy.querySelector('issuerTradingSymbol').text}');
+    bdy.querySelectorAll('nonDerivativeTransaction')
+        .forEach((transactions) {
+      if (transactions
+          .querySelector('transactionCode')
+          .text == tranCode)
+      print(' ${transactions
+          .querySelector('transactionCode')
+          .text} ${transactions
+          .querySelector('transactionShares')
+          .text
+          .trim()}');
+    });
+
+  }
+
+  void reportList(String x) async {
+
+      var req = await _client
+          .getUrl(Uri.parse(x));
+      var resp = await req.close();
+      var pge = await resp.toList();
+      var bdy = parse(utf8.decode(pge[0])).body;
+      
+      bdy.querySelectorAll('entry').forEach((entries){
+        if (entries.querySelector('title').text.contains(('(Issuer)'))) {
+          print('${entries
+              .querySelector('title')
+              .text}');
+          xmlFileAddress('${entries.querySelector('link').attributes['href']}');
+
+        }
+      });
+    }
+
+  void xmlFileAddress(String x) async{
+    var req = await _client
+        .getUrl(Uri.parse(x));
+    var resp = await req.close();
+    var pge = await resp.toList();
+
+    var bdy = parse(utf8.decode(pge[1])).body;
+    //print('https://abc.com${bdy.querySelectorAll('a')[1].attributes['href']}');  
+    transCode('https://abc.com${bdy.querySelectorAll('a')[1].attributes['href']}');
 
   }
