9785879cc83875a0759ba886684eb6314d6b9b41
mdy123 <a_a_aung@yahoo.com>
2020-05-21 16:43:42 -0700

    Update firebase_messaging.dart

--- flutter/firebase_messaging.dart
+++ flutter/firebase_messaging.dart
@@ -1,6 +1,178 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
+void main() => runApp(MaterialApp(
+      home: MyApp(),
+    ));
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final String _fcnAddress = 'https://fcm.googleapis.com/fcm/send';
+  final String _header = '"Content-Type:application/json"';
+  final String _serverKey =
+      "Authorization: key=AAAAx9eHzBY:APA91bFdb0cLCjUH4BrXl3BMmq1qEZBSIXf_WmpNvy3jYHAmtH0pifPmuyuOHnCguMWt2x1eNj9OtvIQtXYZJkQ0OfDvAuwlqrNj1g_eL--gU6z_DPnF_gmTR4sbejpwWLEMVQKElysq";
+
+  //static String _deviceKey = "cq-1WzGNCHo:APA91bE-iEytWF1dKybuJescbJnhmdPYmv73sfAkmp8WhVWgDW0xBhEcJNCcQHm5ErASFZS-9fk2YGFl40MghmWgS7LkYJCNBfnIrxroro_ydoUFuUyG4FVXX96jpki63kiIXxWGGfYi";
+  //String _data = '{"notification": {"body": "this is a body", "title": "title............"}, "priority": "high", "data": {"clickaction": "FLUTTERNOTIFICATIONCLICK", "id": "1", "status": "done"}, "to": $_deviceKey}';
+//  String _data =
+//      '{"notification": {"body": "this is a body", "title": "title............"}, "priority": "high", "data": {"clickaction": "FLUTTERNOTIFICATIONCLICK", "id": "1", "status": "done"}, "to":';
+  //{"notification": {"body": "this is a body", "title": "title............"}
+
+    String _data =
+      ', "priority": "high", "data": {"clickaction": "FLUTTERNOTIFICATIONCLICK", "id": "1", "status": "done"}, "to":';
+
+  //String _curl = '';
+  Map<String,dynamic> _message = {};
+  String _token;
+  var txtFieldController = List.generate(2, (i) => TextEditingController());
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  _register() {
+    _firebaseMessaging.getToken().then((token) {
+      _token = token;
+
+
+
+    });
+  }
+
+  void getMessage() {
+    _firebaseMessaging.configure(
+        onMessage: (Map<String, dynamic> message) async {
+      print('on message ----- $message');
+      //setState(() => _message = message["notification"]["title"] + message["notification"]["body"]);
+      setState(() {
+        _message =
+            message;
+        showAlertDialog(context);
+      });
+
+    }, onResume: (Map<String, dynamic> message) async {
+      print('on resume ----- $message');
+      //setState(() => _message = message["notification"]["title"] + message["notification"]["body"]);
+      setState(() {
+        _message =
+            message;
+
+      });
+      showAlertDialog(context);
+    }, onLaunch: (Map<String, dynamic> message) async {
+      print('on launch ----- $message');
+      //setState(() => _message = message["notification"]["title"] + message["notification"]["body"]);
+      setState(() {
+        _message = message;
+
+      });
+      showAlertDialog(context);
+    });
+  }
+
+  @override
+  void dispose() {
+    txtFieldController.forEach((v)=>v.dispose());
+    super.dispose();
+  }
+  @override
+  void initState() {
+    _register();
+    getMessage();
+    super.initState();
+
+  }
+
+
+  showAlertDialog(BuildContext context) {
+    // set up the button
+    Widget okButton = FlatButton(
+      child: Text("OK"),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text(_message['notification']['body']),
+      content: Text(_message['notification']['title']),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal:35),
+              child: TextField(
+
+                controller: txtFieldController[0],
+                decoration: InputDecoration(
+                    //border: InputBorder.none,
+                    hintText: 'Notification Title'
+                ),
+
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal:35),
+              child: TextField(
+                controller: txtFieldController[1],
+                decoration: InputDecoration(
+                    //border: InputBorder.none,
+                    hintText: 'Notification Body'
+                ),
+              ),
+            ),
+
+            FlatButton(
+              onPressed: (){
+                var _notificationData = '{"notification": {"body": "${txtFieldController[0].text}", "title": "${txtFieldController[1].text}"}';
+
+                print('curl $_fcnAddress -H $_header -X POST -d \'$_notificationData $_data "$_token" }\'  -H "$_serverKey"');
+
+              },
+              color: Theme.of(context).accentColor,
+              child: Text('Generate CURL statement'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
+
+import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
